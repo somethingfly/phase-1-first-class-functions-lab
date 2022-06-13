de1e3c5fe28c67cf082766118a0f6c452cc51492
Alfred <somethingfly@somethingfly.com>
2022-06-12 18:22:17 -0700

    commit tree b8ca69b3a8151c6e8843cb1e2dfba54c0d18aa74

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (array) {
+    const newArray = array.slice(0,2);
+    return newArray;
+}
+
+const returnLastTwoDrivers = function (array) {
+    const newArray = array.slice(-2);
+    return newArray;    
+}
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    function fareMultiplier(fare) {
+        return fare * integer;
+    }
+    return fareMultiplier;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers,returnFunction) {
+    return returnFunction(drivers);
+}
